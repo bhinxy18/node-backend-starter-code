c4d6b6450c600bb817da26c58aee6a901503be77
bhinxy18 <bhinxy18@yahoo.com>
2016-02-13 01:09:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/favorites', function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port 3000");
 });
 
